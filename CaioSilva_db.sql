57de204e2ca82c226fdd257d15291c3680ccfc3b
Caio Vinicius <caioviniaires@gmail.com>
2025-02-25 21:03:44 -0300

    Banco do Crud
    Criação do Doc banco Crud

---
+++ CaioSilva_db.sql
@@ -0,0 +1,12 @@
+
+create database CaioSilva_db
+
+Create table usuario(
+idusuario int not null auto_increment,
+nome text,
+telefone text,
+usuario text,
+senha text,
+primary key(idusuario)
+
+);
